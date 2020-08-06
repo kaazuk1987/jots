c81ad9689c47323efb6429d86d506616fefec003
uk <uk@debian.abc>
2020-08-06 16:57:24 +0200

    losowanie animek

--- script.js
+++ script.js
@@ -2,12 +2,11 @@ $(document).ready(function(){
   $("#cel").click(function(){
   $("#cel").toggle();
 });
-  let x = $("#cel").css("left");
+  let anim=['example','example1'];
+  let x = anim[Math.floor(Math.random()*2)];
   console.log(x);
-  let i=400;
-  for (i;i<=400;i++)
-  	{	
-  		console.log(i);
-  		// $("#cel").css("left",i).delay(2000);
-  	};
+
+
+  
+  
 });
