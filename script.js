79299d93e029c9ba596180ec8b454a88c5a4700e
uk <uk@debian.abc>
2020-08-06 16:41:43 +0200

    biedna anim dziala

--- script.js
+++ script.js
@@ -2,4 +2,12 @@ $(document).ready(function(){
   $("#cel").click(function(){
   $("#cel").toggle();
 });
+  let x = $("#cel").css("left");
+  console.log(x);
+  let i=400;
+  for (i;i<=400;i++)
+  	{	
+  		console.log(i);
+  		// $("#cel").css("left",i).delay(2000);
+  	};
 });
