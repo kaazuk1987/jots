ad76a6ac9c7d6a2373990c8b0b6882a440e0a334
uk <uk@debian.abc>
2020-08-07 18:48:39 +0200

    drobny start jeszcze do oskryptowania

--- script.js
+++ script.js
@@ -3,8 +3,10 @@ $(document).ready(function(){
   $("#cel").toggle();
 });
   let anim=['example','example1'];
-  let x = anim[Math.floor(Math.random()*2)];
+  let x = anim[Math.floor(Math.random()*2)];//losowanie animacji
   console.log(x);
   $("#cel").css("animation-name",x);
  
+//punktacja
+
 });
